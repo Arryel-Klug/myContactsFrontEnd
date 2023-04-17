3f48847b0eb2b306c2599eff82dc1cc12fc1c7ae
Arryel-Kauhu <arryel.klug@gmail.com>
2023-04-17 20:30:23 -0300

    Acessando o projeto pelo celular e ajustes no mobile

--- src/components/Select.js
+++ src/components/Select.js
@@ -11,6 +11,7 @@ export default styled.select`
   outline: none;
   padding: 0 16px;
   font-size: 16px;
+  appearance: none;
 
   &:focus{
     border-color: ${({ theme }) => theme.colors.primary.main};
