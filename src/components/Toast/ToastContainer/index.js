bce5b3ee86c023a539999b813dbcce40eeb1363a
Arryel-Kauhu <arryel.klug@gmail.com>
2023-04-24 10:10:20 -0300

    Trabalhando com variants no styled components

--- src/components/Toast/ToastContainer/index.js
+++ src/components/Toast/ToastContainer/index.js
@@ -1,13 +1,24 @@
+import { useState } from 'react';
 import { Container } from './styles';
 
 import ToastMessage from '../ToastMessage';
 
 export default function ToastContainer() {
+  const [messages] = useState([
+    { id: Math.random(), type: 'default', text: 'Default Text' },
+    { id: Math.random(), type: 'danger', text: 'Danger Text' },
+    { id: Math.random(), type: 'success', text: 'Success Text' },
+  ]);
+
   return (
     <Container>
-      <ToastMessage text="Default toast" />
-      <ToastMessage text="Error toast" type="danger" />
-      <ToastMessage text="Success toast" type="success" />
+      {messages.map((message) => (
+        <ToastMessage
+          key={message.id}
+          type={message.type}
+          text={message.text}
+        />
+      ))}
     </Container>
   );
 }
