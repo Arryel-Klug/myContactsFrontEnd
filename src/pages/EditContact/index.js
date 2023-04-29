86ff2edc88745197c27dd5f2e0630d34de7860f4
Arryel-Kauhu <arryel.klug@gmail.com>
2023-04-29 12:59:10 -0300

    Carregando os dados do contato na página de edição

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,14 +1,48 @@
+import { useParams, useHistory } from 'react-router-dom';
+
+import { useEffect, useState } from 'react';
 import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
+import Loader from '../../components/Loader';
+import toast from '../../utils/toast';
+
+import ContactsService from '../../services/ContactsService';
 
 export default function EditContact() {
+  const [isLoading, setIsLoading] = useState(true);
+  const { id } = useParams();
+  const history = useHistory();
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contactData = await ContactsService.getContactById(id);
+        console.log(contactData);
+        setIsLoading();
+      } catch {
+        history.push('/');
+        toast({
+          type: 'danger',
+          text: 'Contato não encontrado',
+        });
+      }
+    }
+
+    loadContact();
+  }, [id, history]);
+
+  function handleSubmit() {
+    //
+  }
   return (
     <>
+      <Loader isLoading={isLoading} />
       <PageHeader
         title="Editar Mateus Silva"
       />
       <ContactForm
         buttonLabel="Salvar alterações"
+        onSubmit={handleSubmit}
       />
     </>
   );
